33b3e516e109eccfe1565f10ad335b5834e53ea9
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-05-17 13:06:17 +0500

    Created login and register api

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ app.use((req, res, next)=>{
     next(); 
 })
 
+app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT || 3000, ()=> {
