effeaa22cd28904f01c212a8f7c4eab2d933bea6
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-05-17 17:55:14 +0500

    Done with Cart API

---
+++ backend/routes/cart.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const Cart= require('../models/Cart')
+const router = express.Router()
+const {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
+  } = require("./verifyToken");
+
+
+router.post('/', verifyToken, (req,res)=>{
+    const newCart = new Cart(req.body)
+
+    newCart
+        .save()
+        .then(Cart=>{
+            res.status(201).json(Cart)
+        })
+        .catch(err=>{
+            res.status(500).json(err)
+        })
+})
+
+
+
+router.put('/:id', verifyToken, (req,res)=>{
+    
+
+    Cart.findByIdAndUpdate(req.params.id, {
+        $set: req.body //take everything from body and set it again, simple
+    }, {new:true})
+    .then(updatedCart=>{
+        res.status(200).json(updatedCart)
+    })
+    .catch(err=>{
+        res.status(500).json(err)
+    })
+})
+
+// //DELETE Cart
+
+router.delete('/:id', verifyToken, (req,res)=>{
+    Cart.findByIdAndDelete(req.params.id)
+        .then(deletedCart=>{
+            res.status(200).json("User has been deleted...")
+        })
+        .catch(err=>{
+            res.status(500).json(err)
+        })
+})
+
+// //GET USER Cart
+
+router.get('/find/:userid', verifyTokenAndAuthorization,(req,res)=>{
+    Cart.findOne({userId:req.params.userid})
+        .then(Cart=>{
+            res.status(200).json(Cart)
+        })
+        .catch(err=>{
+            res.status(500).json(err)
+        })
+})
+
+//GET ALL CARTS FOR ADMIN
+
+router.get('/', verifyTokenAndAdmin, (req,res)=>{
+    Cart
+    .find()
+    .then(carts=>{
+        res.status(200).json(carts)
+    })
+    .catch(err=>{
+        res.status(500).json(err)
+    })
+})
+
+module.exports = router;
