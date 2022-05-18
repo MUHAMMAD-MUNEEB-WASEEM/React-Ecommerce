8bc8879ac1b1b62ac744689090c69eef557dcdab
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-05-18 11:07:25 +0500

    added stripe

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-
+const cors = require('cors')
 
 //routes
 const userRoute = require('./routes/user')
 const productRoute = require('./routes/product')
 const orderRoute = require('./routes/order')
 const authRoute = require('./routes/auth')
+const stripeRoute = require('./routes/stripe')
 
 dotenv.config();
 
@@ -18,26 +19,17 @@ mongoose
     .catch(()=>console.log("Error"))
 
 
+app.use(cors())
+
 //body parser which is now replaced by express is used to parse body of your choice
 app.use(express.urlencoded({extended: false}))//parse simple bodies of url encoded data
 app.use(express.json());//this is to extract json data and make it readable
-
-//Insurance to prevent cors error
-app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-    if (req.method === 'OPTIONS'){
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next(); 
-})
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log("Backend Server is running!")
